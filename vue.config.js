a6fd5c6b144c046250598b507a8507022409c094
ann <ann110149@gmail.com>
2021-12-21 18:57:28 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+module.exports = {
+  lintOnSave: false,
+  publicPath: '.',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
+        '@': path.resolve('src'),
+        src: path.resolve('src'),
+        utils: path.resolve('src/utils'),
+        styles: path.resolve('src/styles'),
+        assets: path.resolve('src/assets'),
+        components: path.resolve('src/components'),
+        services: path.resolve('src/services'),
+        'vuex-store': path.resolve('src/store'),
+      },
+    },
+  },
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+    },
+  },
+  devServer: {
+    disableHostCheck: true,
+  },
+};

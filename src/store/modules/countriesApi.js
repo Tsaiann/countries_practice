420ca7bb6642c91f067575838d15d9aa94551074
ann <ann110149@gmail.com>
2021-12-22 22:11:36 +0800

    update

--- src/store/modules/countriesApi.js
+++ src/store/modules/countriesApi.js
@@ -10,11 +10,19 @@ const countriesModule = {
   },
 
   mutations: {
-
+    SET_RES(state, payload){
+      state.res = payload
+    }
   },
 
   actions: {
-    
+    async GET_API(context, payload){
+      const apiUrl ="https://restcountries.com/v2/all"
+      const res= await api.get(apiUrl)
+      payload = res
+      context.commit('SET_RES', payload)
+      console.log(res,payload)
+    }
   }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="home">
+    <div class="row horizontal space">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="100">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-
+import { mapMutations, mapGetters, mapActions } from 'vuex'
 export default {
-  name: 'Home'
+  name: 'Home',
+  date(){
+
+  },
+  computed:{
+    ...mapGetters([])
+  },
+  methods:{
+    ...mapActions(['GET_API']),
+    ...mapMutations(['SET_RES']),
+
+    async callApi(){
+      this.GET_API()
+    }
+  },
+  created(){
+    this.callApi()
+  }
 }
 </script>
